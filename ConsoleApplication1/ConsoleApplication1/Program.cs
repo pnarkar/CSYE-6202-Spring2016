5fd4ad52899b1c5f1b06911997b5fbfe4ab222ff
pnarkar <narkar.p@husky.neu.edu>
2016-02-07 20:30:20 -0500

    Assignment1

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+
+            int x = int.Parse(Console.ReadLine());
+            if (x % 3 == 0 && x % 5 == 0)
+            {
+                System.Console.WriteLine("FizzBuzz");
+
+            }
+            else if (x % 5 == 0)
+            {
+                System.Console.WriteLine("Buzz");
+
+            }
+
+            else if (x % 3 == 0)
+            {
+                System.Console.WriteLine("Fizz");
+
+            }
+            else {
+                System.Console.WriteLine(x);
+            }
+        }
+    }
+}
